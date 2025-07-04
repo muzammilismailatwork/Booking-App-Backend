5b278f8cf1bffa4852c3abb8699ea6b9fdb7062c
Muzammil Ismail <muzammilsoomro4321@gmail.com>
2025-07-04 23:46:03 +0500

    auth

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,29 @@
 import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+router.get("/checkauthentication", verifyToken, (req, res, next)=>{
+    res.send("hello user, you are logged in")
+})
 
+router.get("/checkuser/:id", verifyUser , (req, res, next)=>{
+    res.send("hello user, you are logged in and you can delete your account!")
+})
+
+//Update
+
+router.put("/:id", updateUser)
+
+//Delete
+
+router.delete("/:id", deleteUser)
+
+// get single User
+router.get("/:id", getUser)
+
+// Get all Users
+router.get("/", getUsers)
 
 export default router
